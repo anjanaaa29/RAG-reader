56c0a81d56fba7494ad250769303fd56e275f0f8
Anjana Suresh <121029167+anjanaaa29@users.noreply.github.com>
2025-10-18 18:53:58 +0530

    Update vector_db.py

--- src/vector_db.py
+++ src/vector_db.py
@@ -2,7 +2,7 @@ from langchain_community.vectorstores import FAISS
 # from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from typing import List, Optional, Dict, Any
-from langchain_core.documents import Document
+from langchain.schema import Document
 import os
 import pickle
 
