2420c0735bc2673659d035a43553f7497fbf0c57
Anjana Suresh <121029167+anjanaaa29@users.noreply.github.com>
2025-10-18 12:41:38 +0530

    Update doc_loader.py

--- src/doc_loader.py
+++ src/doc_loader.py
@@ -1,7 +1,7 @@
 import os
 import json
 from typing import List, Dict, Union
-from langchain.schema import Document
+from langchain_core.documents import Document
 from langchain_community.document_loaders import (
     PyPDFLoader,
     UnstructuredWordDocumentLoader,
