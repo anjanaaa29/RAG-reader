ba37bd02f1f0f60fee62c6c98797e1a23f0fc606
Anjana Suresh <121029167+anjanaaa29@users.noreply.github.com>
2025-10-18 13:00:56 +0530

    Fix import statement for create_retrieval_chain

--- src/retrieval_chain.py
+++ src/retrieval_chain.py
@@ -1,4 +1,4 @@
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
